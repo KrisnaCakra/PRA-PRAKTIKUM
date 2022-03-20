0b198cf8f145a46817ea44913f57c6426708df58
krisna cakra <krisnacningrat@gmail.com>
2022-03-20 10:35:26 +0700

    JAJAL KALKULATOR

---
+++ PRA PRAKTIKUM/TUGAS/KALKULATOR.cpp
@@ -0,0 +1,37 @@
+/* 
+KELOMPOK PRA PRAKTIKUM ALGORITMA PEMROGRAMAN 
+ANGGOTA :
+
+KRISNA CAKRA NINGRAT - 2100018449
+ANELA INDRA - 2100018454
+SASQIA AULIA NUR AINI - 2100018456
+*/
+
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int bil1, bil2, pil;
+  float hasil;
+  string operasi;
+  
+  cout << "TUGAS KELOMPOK PRATIKUM ALGORITMA PEMROGRAMAN" << endl;
+  cout << "------------------KALKULATOR-----------------" << endl;
+  cout << "1. Penjumlahan" << endl;
+  cout << "2. Pengurangan" << endl;
+  cout << "3. Perkalian" << endl;
+  cout << "4. Pembagian" << endl;
+  cout << "5. Modulus" << endl;
+  cout << "6. Perpangkatan" << endl;
+  cout << "7. Akar (pilih nilai pada bilangan dua)" << endl;
+  cout << "\n\nMasukkan Pilihan : ";
+cin >> pil;
+  cout << "Masukkan Bilangan Pertama : ";
+  cin >> bil1;
+  cout << "Masukkan Bilangan Kedua : ";
+  cin >> bil2;
+
+ 
